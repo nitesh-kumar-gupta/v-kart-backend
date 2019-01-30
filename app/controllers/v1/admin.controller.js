9648d9370dfbce19a07e44cbd7fa16af60f52307
Nitesh Kumar Gupta <nitesh.gupta@venturepact.com>
2019-01-30 18:11:37 +0530

    keys

--- app/controllers/v1/admin.controller.js
+++ app/controllers/v1/admin.controller.js
@@ -12,3 +12,8 @@ module.exports = {
         return response.success(res, constants.success.CREATED, productCatagory);
     }
 };
+
+// IMGUR_CLIENT_ID = 'da79431e16ac878'
+// IMGUR_CLIENT_SECRET = 'aba08ad48fc7dc505fedfcb7c48accdf300f094a'
+// ff1095201f7f6bebed754bed6a326770375492c5
+// d0b765d706279dc5815f58de7c723974d7dea971
